307cce93319e2c4e282818699aa156ea7941c3f7
Adam Dubiel <dubiel.adam@gmail.com>
2013-10-20 20:09:51 +0200

    smartparam + spring demos

---
+++ src/test/java/org/smartparam/demo/spring/SpringDemo.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2013 Adam Dubiel, Przemek Hertel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.smartparam.demo.spring;
+
+import org.smartparam.engine.core.context.DefaultContext;
+import org.smartparam.engine.core.engine.ParamEngine;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
+import static org.fest.assertions.api.Assertions.assertThat;
+
+/**
+ *
+ * @author Adam Dubiel
+ */
+@ContextConfiguration(locations = "classpath:/spring/demo-context.xml")
+public class SpringDemo extends AbstractTestNGSpringContextTests {
+
+    @Autowired
+    private ParamEngine paramEngine;
+
+    @Test
+    public void demonstrateUsingParamEngineAsSpringBean() {
+        // given
+
+        // when
+        String value = paramEngine.get("simpleParameter", "HELLO_WORLD", 1).getString();
+
+        // then
+        assertThat(value).isEqualTo("Hello World");
+    }
+
+    @Test
+    public void demonstrateSpringPluginsInDynamicContext() {
+        // given
+
+        // when
+        DefaultContext context = new DefaultContext();
+        /*
+         * Passing context declares usage of dynamic level creators. Only
+         * input level in dayToMonthMapping parameter is "date" level with
+         * "date" level creator defined in SpringLevelCreators. It returns
+         * date from DateProvider, which is always 2013-04-15.
+         */
+        String value = paramEngine.get("dayToMonthMapping", context).getString();
+
+        // then
+        assertThat(value).isEqualTo("April 2013");
+
+    }
+}
